f1498e96e2751e4997aafadf21f0ac43fec6cecb
Joe Lou <realmx2000@DN0a1f8330.SUNet>
2020-02-22 17:57:37 -0800

    initial dataset

--- datasets/equation_dataset.py
+++ datasets/equation_dataset.py
@@ -1,7 +1,7 @@
 import torch
 import numpy as np
 import json
-import gensim
+import re
 
 from torch.utils.data import Dataset
 
@@ -14,19 +14,102 @@ class EquationDataset(Dataset):
         PREFIX = 'data/microsoft/'
         self.questions = []
         self.equations = []
+        self.max_num_variables = 0
+        self.max_num_constants = 0
+
         for filename in names:
             with open(PREFIX + filename, 'r') as file:
                 data = json.load(file)
             for example in data:
-                question = data['sQuestion'].split(' ')
-                equation = data['lEquations']
+                question = example['sQuestion']
+                equation_system = example['lEquations']
 
-                num_index = 0
-                for idx, word in enumerate(question):
-                    if word.isnumeric():
-                        question[idx] = 'n' + str(num_index)
+                const_dict = self.replace_constants(question, {})
+                question = question.split(' ')
+
+                var_label = 'a'
+                var_dict = {}
+                for equation in equation_system:
+                    var_dict = self.replace_variables(equation, var_dict, var_label)
+
+                    self.replace_constants(equation, const_dict)
+
+                self.questions.append(question)
+                self.equations.append(equation_system)
+                self.max_num_constants = max(self.max_num_constants, len(const_dict))
+                self.max_num_variables = max(self.max_num_variables, len(var_dict))
+
+    @staticmethod
+    def replace_constants(string: str, const_dict: dict, const_label: int) -> dict:
+        '''
+        Replace all numeric constants in the string with number tokens.
+        Args:
+            string: String to process.
+            const_dict: Dictionary of constant -> number token mappings, to be used
+                where possible.
+            const_label: The constant token to start from for unseen constants
+
+        Returns:
+            const_dict: updated dictionary of constant -> number token mappings.
+        '''
+        # Replace all numeric constants with number tokens.
+        constant_re = re.compile('[0-9.]+')
+        constants = constant_re.finditer(string)
+        for match in constants:
+            const_val = float(match.group())
+            if const_val in const_dict:
+                constant =  const_dict[const_val]
+            else:
+                constant = '<n' + str(const_label) + '>'
+                const_label += 1
+                const_dict[const_val] = constant
+            string = string[:match.start()] + constant + string[match.end()]
+
+        return const_dict
+        '''
+        num_index = 0
+        const_dict = {}
+        for idx, word in enumerate(str):
+            if word.isnumeric():
+                if word not in const_dict:
+                    token = '<n' + str(num_index) + '>'
+                    str[idx] = token
+                    const_dict[float(word)] = token
+                else:
+                    str[idx] = const_dict[float(word)]
+        '''
+
+    @staticmethod
+    def replace_variables(equation: str, var_dict: dict, var_label: str):
+        """
+        Replace the variables in an equation with variable tokens.
+        Args:
+            equation: The equation to process.
+            var_dict: Dictionary of var -> var token mappings, to be followed where possible.
+            var_label: the label to start from for unseen variables.
+
+        Returns:
+            var_dict: the mapping of variables to variable tokens.
+        """
+        variable_re = re.compile('[^0-9.()+\-*/^]+')
+
+        variables = variable_re.finditer(equation)
+        for match in variables:
+            var_name = match.group()
+            if var_name in var_dict:
+                var = var_dict[var_name]
+            else:
+                var = var_label
+                var_dict[var_name] = var
+                var_label = chr(ord(var_label) + 1)
+            equation = equation[:match.start()] + var + equation[match.end():]
+        return var_dict
 
     def __getitem__(self, idx):
         return self.questions[idx], self.equations[idx]
+
+    def __len__(self):
+        return len(self.questions)
+
 if __name__ == '__main__':
     EquationDataset(['kushman.json'])
